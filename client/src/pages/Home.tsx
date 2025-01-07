8f0c66b2fee8f77babbfedd2cb391dadd2ff6c4f
Danielh74 <danielh07042000@gmail.com>
2025-01-07 19:19:51 +0200

    update homepage branding and content; change title to DateScape, modify welcome message, and enhance navigation links

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,38 +1,41 @@
+import { Link } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
 import '../styles/home.css'
 
 const Home = () => {
+    const { currentUser, handleLogout } = useAuth();
     return (
         <div className='d-flex vh-100 text-white bg-dark body'>
             <div className="container">
                 <div className="row h-100">
                     <header className="col-12 p-3">
                         <div className="row justify-content-between">
-                            <h3 className="col">YelpCamp</h3>
+                            <h3 className="col">DateScape</h3>
                             <nav className="nav nav-masthead col-12 col-md-6 justify-content-md-end">
                                 <a className="nav-link active" id='nav-link' href="">Home</a>
-                                <a className="nav-link" id='nav-link' href="/campgrounds">Campgrounds</a>
-                                {/* {!currentUser ? (
-                            <>
-                                <a className="nav-link" href="/login">Login</a>
-                                <a className="nav-link" href="/register">Register</a>
-                            </>
-                        ) : (
-                            <a className="nav-link" href="/logout">Logout</a>
-                        )} */}
+                                <Link className="nav-link" id='nav-link' to="/campgrounds">Locations</Link>
+                                {!currentUser ? (
+                                    <>
+                                        <Link className="nav-link" id='nav-link' to="/login">Login</Link>
+                                        <Link className="nav-link" id='nav-link' to="/register">Register</Link>
+                                    </>
+                                ) : (
+                                    <Link id='nav-link' className="nav-link" to={'#'} onClick={handleLogout}>Logout</Link>
+                                )}
                             </nav>
                         </div>
                     </header>
                     <main className="col align-self-center text-center">
-                        <h2 className="mb-3">Welcome to YelpCamp!</h2>
+                        <h2 className="mb-3">Welcome to DateScape!</h2>
                         <p className="lead">
-                            Discover Amazing Campgrounds Around the World! <br />
-                            Whether you're planning your next outdoor adventure or just exploring, connect with
-                            stunning camping destinations across the globe. <br />
-                            Start your journey today — explore, plan, and make memories in nature.
+                            Discover Perfect Date Spots Near You. <br />
+                            Looking for a unique spot to impress your date? Whether it’s your first time out or a special
+                            anniversary, DateScape helps you find the perfect setting for unforgettable moments. <br />
+                            From dreamy hideaways to exciting outings — make every date a story worth sharing.
                         </p>
-                        <a className="btn btn-secondary" href="/campgrounds">Go To Campgrounds</a>
+                        <a className="btn btn-secondary" href="/campgrounds">Find Your Next Date Spot</a>
                     </main>
-                    <footer className="mt-auto pb-2">&copy; 2024 YelpCamp</footer>
+                    <footer className="mt-auto pb-2">&copy; 2024 DateScape</footer>
                 </div>
             </div>
         </div>
